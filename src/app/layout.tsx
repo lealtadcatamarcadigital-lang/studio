3d3a07931258eca988667ca7b88d008c22351cec
Lealtad Comercial Catamarca Digital <lealtadcatamarcadigital@gmail.com>
2025-10-14 14:03:50 +0000

    no puedo hacer commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="https://i.imgur.com/ITpm1XW.png" type="image/png" />
-        <link rel="apple-touch-icon" href="https://i.imgur.com/ITpm1XW.png" />
+        <link rel="icon" href="https://i.imgur.com/S6Imh3m.png" type="image/png" />
+        <link rel="apple-touch-icon" href="https://i.imgur.com/S6Imh3m.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
--- src/lib/wrestlers-data.ts
+++ src/lib/wrestlers-data.ts
@@ -1,3 +1,4 @@
+
 export interface WrestlerData {
   imageUrl?: string;
 }
