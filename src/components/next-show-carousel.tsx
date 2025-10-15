5ebd3e971f4588079c7fd065d04ea8ba55927cf2
Lealtad Comercial Catamarca Digital <lealtadcatamarcadigital@gmail.com>
2025-10-15 12:29:00 +0000

    no sale el carrusel en la vista mobil

--- src/components/next-show-carousel.tsx
+++ src/components/next-show-carousel.tsx
@@ -98,8 +98,8 @@ export function NextShowCarousel({ events, onEventSelect, eventStatuses, onToggl
                     )
                 })}
                 </CarouselContent>
-                <CarouselPrevious className="hidden sm:flex" />
-                <CarouselNext className="hidden sm:flex" />
+                <CarouselPrevious className="sm:flex -left-4" />
+                <CarouselNext className="sm:flex -right-4" />
             </Carousel>
         </div>
     </div>
